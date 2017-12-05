df267393f4f6a07ea5a9cecb6298201e6634e39f
mrtehnuke <davidsgabalins@gmail.com>
2017-12-05 18:29:28 +0000

    added READ btn to blog index page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,14 +4,17 @@
     <h1>Posts</h1>
     @if(count($posts) > 0)
         @foreach($posts as $row)
-            <div class="card card-body">
-                <div class="row">
+            <div class='card card-body'>
+                <div class='row flex-row'>
                     <div class='col-sm-4'>
                         <img style='width:100%' src='/storage/cover_images/{{$row->cover_image}}'>
                     </div>
-                    <div class='col-sm-8'>
-                        <h3><a href="/posts/{{$row->id}}">{{$row->title}}</a></h3>
-                        <small>Written on {{$row->created_at}} by {{$row->user->name}}</small>
+                    <div class='col-sm-8 d-flex flex-column align-items-start'>
+                        <div>
+                            <h3>{{$row->title}}</h3>
+                            <small>Written on {{$row->created_at}} by {{$row->user->name}}</small>
+                        </div>
+                        <a class='btn btn-dark mt-auto' href='/posts/{{$row->id}}'>Read</a>
                     </div>
                 </div>
             </div>
