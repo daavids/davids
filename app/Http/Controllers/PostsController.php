76af6736afee4c443786fbe8517a6dc00282c778
mrtehnuke <davidsgabalins@gmail.com>
2017-12-05 22:13:57 +0000

    changes in pagination

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -23,7 +23,7 @@ class PostsController extends Controller
     {
         // $posts = Post::all();
         //$posts = Post::orderBy('title', 'asc')->get();
-        $posts = Post::orderBy('created_at','desc')->paginate(10);
+        $posts = Post::orderBy('created_at','desc')->paginate(3);
         return view('posts.index')->with('posts', $posts);
     }
 
