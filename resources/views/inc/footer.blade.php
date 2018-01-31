769616253bec56f7ddaf1bfb6790190acac5097c
mrtehnuke <davidsgabalins@gmail.com>
2018-01-31 19:12:01 +0200

    fixed footer copyright/date

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,3 +1,3 @@
 <nav class='navbar navbar-dark navbar-expand-sm footer fixed-bottom'>
-    <h5 class='mx-auto'>Dāvids Gabaliņš 2017<h5>
+    <h5 class='mx-auto'>&copy;Dāvids Gabaliņš <?= date("Y")?><h5>
 </nav>
